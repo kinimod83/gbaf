05d6bcb3fa9054b508a9ac228b4c515870aebae4
kinimod83 <fkinimod@free.fr>
2023-01-01 14:07:31 +0100

    ajout de fichiers

---
+++ home.php
@@ -0,0 +1,53 @@
+<!-- ne pas oublier le session_start() -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">    
+    <title>GBAF - Page d'accueil</title>
+    <link href="styles.css" rel="stylesheet">    
+</head>
+<body>
+
+    
+
+    <!-- Header -->
+    <?php include_once('header.php'); ?>
+
+    <?php init_acteurs_table(); ?>
+
+    <!-- Section Presentation -->
+    <section class="section-presentation">
+        <h1> Texte de présentation de GBAF</h1>
+    </section>
+
+    <!-- ligne horizontale -->
+    <hr>
+
+    <!-- Section Acteur -->
+    <section class="section-acteurs">
+        <h2> Texte de présentation de la section acteurs</h2>
+
+        <?php for ($i = 0; $i < $nbActeurs; $i++):?>
+
+            <div class=un-acteur>
+            
+                <?php $ligneActeur = $acteursTable[$i]; ?> 
+
+                <?php $imagePath = $rootPath.'/images/'.$ligneActeur['logo'];  ?>
+
+                <img src="<?php echo $imagePath; ?>" alt="Logo Acteur" class="logo-acteur">
+
+                <h3 class=texte-tronque> <?php echo $ligneActeur['description'];?> /<h3>
+
+                <a class="lire-la-suite" href="https://openclassrooms.com/">Lire la suite</a>
+            </div>
+
+        <?php endfor; ?>
+
+    </section>    
+
+    <!-- Footer -->
+    <?php include_once('footer.php'); ?>
+</body>
+</html>
